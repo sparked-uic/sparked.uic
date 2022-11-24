e1edab7a25f7dd3c0e331064def9ad39b0a560e8
Jason Anderson <sparked.uic@gmail.com>
2022-11-24 16:29:46 -0500

    commit

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sparked UIC</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="nav">
+        <a href=""><img src="logo.png" alt=""></a>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="signup.php">Sign Up</a></li>
+            <li><a href="login.php">Log In</a></li>
+        </ul>
+    </div>
+</body>
+</html>
